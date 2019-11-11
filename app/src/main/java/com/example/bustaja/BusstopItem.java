399c4d5147bf5b4c039d8b76de8977c455a62d05
icetea94 <devshyeon@gmail.com>
2019-11-11 10:49:10 +0900

    Merge branch 'hotfix'
    # Conflicts:
    #	app/src/main/java/com/example/bustaja/BusstopItem.java

--- app/src/main/java/com/example/bustaja/BusstopItem.java
+++ app/src/main/java/com/example/bustaja/BusstopItem.java
@@ -1,4 +1,36 @@
 package com.example.bustaja;
 
 public class BusstopItem {
-}
+
+        private String Busstopnum;
+        private String Busstopname;
+
+        public BusstopItem() {
+        }
+
+        public BusstopItem(String Busstopnum, String Busstopname) {
+
+            this.Busstopnum = Busstopnum;
+            this.Busstopname = Busstopname;
+
+        }
+
+        public String getBusstopnum() {
+            return Busstopnum;
+        }
+
+        public void setBusstopnum(String busstopnum) {
+            Busstopnum = busstopnum;
+        }
+
+        public String getBusstopname() {
+            return Busstopname;
+        }
+
+        public void setBusstopname(String busstopname) {
+            Busstopname = busstopname;
+        }
+
+    }
+
+
